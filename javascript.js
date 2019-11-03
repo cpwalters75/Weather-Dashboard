c1db14604c274f88e7a608a78fdc1bfd721cf1d4
Chris Walters <cpwalters75@gmail.com>
2019-11-03 17:25:12 -0500

    restructured HTML, starting to add boot strap elements back in

---
+++ javascript.js
@@ -0,0 +1,42 @@
+$( document ).ready(function() {
+    var userInput ="";
+    $( "#Submit" ).click(function(){
+
+        userInput=$("#cityId").val();
+        console.log(userInput)
+        $.ajax({
+            type: "GET",
+            url: "http://api.openweathermap.org/data/2.5/weather?q="+userInput+"&appid=9ef5a8c0a9f360fcd04b4b54945cddee",
+            success: function(response){
+                console.log(response);
+                $("#cityName").text(response.name);
+                $("#cityTemp").text(response.main.temp);
+                $("#cityHumidity").text(response.main.humidity);
+                $("#cityWS").text(response.wind.speed)
+            }
+
+            
+        })
+    })
+  
+});
+
+$( document ).ready(function() {
+    var userInput ="";
+    $( "#Submit" ).click(function(){
+
+        userInput=$("#cityId").val();
+        console.log(userInput)
+        $.ajax({
+            type: "GET",
+            url: "http://api.openweathermap.org/data/2.5/forecast?q="+userInput+"&appid=9ef5a8c0a9f360fcd04b4b54945cddee",
+            success: function(response){
+                console.log(response);
+              
+            }
+
+            
+        })
+    })
+  
+});
