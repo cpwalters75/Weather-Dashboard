ee9a5f55014a216794e621b40202a76169b781c1
Chris Walters <cpwalters75@gmail.com>
2019-11-03 18:44:02 -0500

    5 day added

--- javascript.js
+++ javascript.js
@@ -40,3 +40,23 @@ $( document ).ready(function() {
     })
   
 });
+
+$( document ).ready(function() {
+    var userInput ="";
+    $( "#Submit" ).click(function(){
+
+        userInput=$("#cityId").val();
+        console.log(userInput)
+        $.ajax({
+            type: "GET",
+            url: "http://api.openweathermap.org/data/2.5/uvi/forecast?l"+userInput+"&appid=9ef5a8c0a9f360fcd04b4b54945cddee",
+            success: function(response){
+                console.log(response);
+              
+            }
+
+            
+        })
+    })
+  
+});
